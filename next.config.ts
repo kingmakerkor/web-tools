cac37c1c9949f32e138f3a88e83379cc2fe3cb6c
kingmaker <peter31jjang@gmamil.com>
2025-08-08 11:56:15 +0900

    practices fix

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,19 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/(:path*)',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
